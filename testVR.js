fe71ff55fccd4e95930092f461562d9fad5fca49
kikpond <kikpond15@gmail.com>
2023-01-10 19:14:02 +0900

    update js

--- testVR.js
+++ testVR.js
@@ -13,8 +13,11 @@ function caluculate(){
 
 function draw() {
     setVRBackgroundColor(255, 255, 255); //RGB3つ設定しないとダメ
-    setViewerPosition(0, 0, 400);
+    //setViewerPosition(0, 0, 400);
 
     fill(255,0,0);
-    rect(0,0,-100, 200, 200);
+    translate(0, 0, 10);
+    rotateX(10);
+    rotateY(20);
+    box(5);
 }
